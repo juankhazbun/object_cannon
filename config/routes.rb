07aef422651767aee465e4d73e24960619e8be3c
Juank Hazbun <juankhazbun@gmail.com>
2015-05-23 21:25:03 -0500

    Add routes for game actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :games, only: [:new, :create, :show]
+
+  get 'games/ranking' => 'games#ranking'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
