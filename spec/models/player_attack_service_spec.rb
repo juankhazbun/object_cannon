b2759e5b6e6f4867e94b8852e38d2dd2f238d796
Juank Hazbun <juankhazbun@gmail.com>
2015-05-24 21:18:40 -0500

    Add test for model player_attack_service

--- spec/models/player_attack_service_spec.rb
+++ spec/models/player_attack_service_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 describe PlayerAttackService do
 
+	before(:each) do
+		# Insert player 1
+		player1 = create(:player)
+
+		# Insert Player 2
+		player2 = create(:player)
+
+		# Create a new game
+		@game = create(:game, player1: player1, player2: player2)
+
+		# Create service to attack
+		@attack_service = PlayerAttackService.new @game.id
+	end
+
 	context 'hitting the opponent'do 
-
-		before(:each) do
-			# Insert player 1
-			player1 = create(:player)
-
-			# Insert Player 2
-			player2 = create(:player)
-
-			# Create a new game
-			@game = create(:game, player1: player1, player2: player2)
-
-			# Create service to attack
-			@attack_service = PlayerAttackService.new @game.id
-		end
 
 		it "player1 hit the opponent" do
 
@@ -50,15 +50,24 @@ describe PlayerAttackService do
 
 			hit_points = @attack_service.attack
 
-			puts @game.player1.wall_health
-
-			puts @game.player2.wall_health
-
 			@game.reload
 
 			expect(@game.winner).to eq(1) if hit_points > 0
 
 			expect(@game.winner).to eq(0) if hit_points == 0
 		end
+
+		it 'the health could not be negative' do
+			# stub the extra hit points to get 20 attributes 
+			allow(@attack_service).to receive(:get_extra_hit_points).and_return(20)
+
+			hit_points = @attack_service.attack
+
+			@game.reload
+
+			expect(@game.player2.wall_health).to eq(0)
+		end
 	end
+
+
 end
